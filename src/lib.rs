594aaf0b525ecf1d8f58440a084ed000c5e45ebf
Nick Jones <nicktjones@pm.me>
2022-02-09 15:39:23 +1100

    Implement Set1/Challenge2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
-use hex::{FromHex, FromHexError};
+use hex::{FromHex};
 use base64::{encode};
 
-pub fn from_hex(h: String) -> Result<Vec<u8>, FromHexError> {
-    Vec::<u8>::from_hex(h)
+pub fn from_hex(h: String) -> Option<Vec<u8>> {
+    Vec::<u8>::from_hex(h).ok()
 }
 
 pub fn to_base64(bb: Vec<u8>) -> String {
     encode(bb)
+}
+
+pub fn xor(a: Vec<u8>, b: Vec<u8>) -> Option<Vec<u8>> {
+    let n = a.len();
+    if b.len() != n {
+        return None
+    }
+
+    let mut c = Vec::<u8>::new();
+    for i in 0..n {
+        c.push(a[i] ^ b[i])
+    }
+
+    return Some(c)
 }
--- tests/set1.rs
+++ tests/set1.rs
@@ -5,9 +5,16 @@ mod tests {
       let hex = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
       let base64 = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
 
-      match matasano::from_hex(hex) {
-        Ok(bb) => assert_eq!(matasano::to_base64(bb), base64),
-        Err(err) => panic!("{}", err),
-      }
+      let bb = matasano::from_hex(hex).unwrap();
+      assert_eq!(matasano::to_base64(bb), base64);
+    }
+
+    #[test]
+    fn challenge2() {
+      let msg = matasano::from_hex("1c0111001f010100061a024b53535009181c".to_string()).unwrap();
+      let mask = matasano::from_hex("686974207468652062756c6c277320657965".to_string()).unwrap();
+      let want = matasano::from_hex("746865206b696420646f6e277420706c6179".to_string()).unwrap();
+
+      assert_eq!(matasano::xor(msg, mask).unwrap(), want)
     }
 }
