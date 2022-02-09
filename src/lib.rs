af43f63d445798c8f4d9a2be6cb773e5ddd684b8
Nick Jones <nicktjones@pm.me>
2022-02-09 15:51:44 +1100

    Implement Set1/Challenge3.

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,25 @@ pub fn to_base64(bb: Vec<u8>) -> String {
     encode(bb)
 }
 
-pub fn xor(a: Vec<u8>, b: Vec<u8>) -> Option<Vec<u8>> {
+pub fn xor(a: &Vec<u8>, b: &Vec<u8>) -> Option<Vec<u8>> {
     let n = a.len();
-    if b.len() != n {
-        return None
-    }
+    let m = b.len();
 
     let mut c = Vec::<u8>::new();
     for i in 0..n {
-        c.push(a[i] ^ b[i])
+        c.push(a[i] ^ b[i % m])
     }
 
     return Some(c)
+}
+
+pub fn count(b: &Vec<u8>, c: u8) -> i32 {
+    let mut sum: i32 = 0;
+
+    for ch in b {
+        if *ch == c {
+            sum += 1;
+        }
+    }
+    return sum
 }
--- tests/set1.rs
+++ tests/set1.rs
@@ -1,20 +1,44 @@
 #[cfg(test)]
 mod tests {
+  use matasano::*;
+  use std::str;
+
     #[test]
     fn challenge1() {
       let hex = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
       let base64 = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
 
-      let bb = matasano::from_hex(hex).unwrap();
-      assert_eq!(matasano::to_base64(bb), base64);
+      assert_eq!(to_base64(from_hex(hex).unwrap()), base64);
     }
 
     #[test]
     fn challenge2() {
-      let msg = matasano::from_hex("1c0111001f010100061a024b53535009181c".to_string()).unwrap();
-      let mask = matasano::from_hex("686974207468652062756c6c277320657965".to_string()).unwrap();
-      let want = matasano::from_hex("746865206b696420646f6e277420706c6179".to_string()).unwrap();
+      let msg = from_hex("1c0111001f010100061a024b53535009181c".to_string()).unwrap();
+      let mask = from_hex("686974207468652062756c6c277320657965".to_string()).unwrap();
+      let want = from_hex("746865206b696420646f6e277420706c6179".to_string()).unwrap();
 
-      assert_eq!(matasano::xor(msg, mask).unwrap(), want)
+      println!("{}", str::from_utf8(&msg).unwrap());
+      println!("{}", str::from_utf8(&mask).unwrap());
+      println!("{}", str::from_utf8(&want).unwrap());
+
+      assert_eq!(xor(&msg, &mask).unwrap(), want)
+    }
+
+    #[test]
+    fn challenge3() {
+      let msg = from_hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".to_string()).unwrap();
+
+      let mut max_spaces = -1;
+      let mut best :Vec<u8> = vec![];
+      for i in 0..=255 {
+        let res = xor(&msg, &vec![i]).unwrap();
+        let n_spaces = count(&res, String::from(' ').as_bytes()[0]);
+        if n_spaces > max_spaces {
+          max_spaces = n_spaces;
+          best = res;
+        }
+      }
+      // Run test with `cargo test -- --nocapture` to see output.
+      println!("Best: {}", str::from_utf8(&best).unwrap());
     }
 }
