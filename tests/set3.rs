81843739ac27b9347c54aca08d402782047ccacb
Nick Jones <nicktjones@pm.me>
2022-06-15 16:01:52 +1000

    wip

---
+++ tests/set3.rs
@@ -0,0 +1,14 @@
+mod tests {
+  use std::collections::HashMap;
+  use std::fs;
+  use std::str;
+
+  use matasano::aes::{Decrypter, Encrypter, CBC, ECB};
+  use matasano::cbc;
+  use matasano::ecb;
+  use matasano::*;
+
+  #[test]
+  fn challenge17() {
+  }
+}
